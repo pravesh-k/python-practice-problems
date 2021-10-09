915a075f94b4fbce407fbcf313655ddc8234fea4
Pravesh <praveshnayak@gmail.com>
2021-10-10 00:20:21 +0530

    optimize: improved speed and memory utilization

--- rotate_array.py
+++ rotate_array.py
@@ -5,9 +5,28 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        
-        for i in range(k):
-            nums.insert(0, nums.pop())
+        # below is a short and simple solution but it is
+        # slow because of complexity of background steps
+
+        # for i in range(k):            
+            # nums.insert(0, nums.pop())
+            
+        # below given is another simple yet better 
+        # solution for the problem statement
+
+        n = len(nums)
+        k = k % n
+        kl, lk = nums[n-k:], nums[:n-k]
+        lkl = kl + lk
+
+        for i in range(n):
+            nums[i] = lkl[i]
+
+        # below given is the simplest and the best performing 
+        # solution in terms of speed but memory performace is bad
+
+        # k %= len(nums)
+        # nums[k:], nums[:k] = nums[:-k], nums[-k:]
 
         return nums
 
